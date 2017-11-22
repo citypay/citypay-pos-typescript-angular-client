8a3de2fee6a4028799dbc1af94dd8556ad9a5035
Gary Feltham <gary.feltham@citypay.com>
2017-11-22 13:40:49 +0000

    initial import

--- model/transactionProgress.ts
+++ model/transactionProgress.ts
@@ -1,6 +1,6 @@
 /**
  * CityPay POS API
- * CityPay Point of Sale API for payment with card present devices including EMV readers and contactless POS readers.  The API makes it simple to add EMV and contactless card acceptance to iOS, Android, Tablet and desktop applicaitons using a HTTPS protocol. It segregates the complexity of payment processing from the sales environment and eliminates any necessity for the target system to handle card data. 
+ * CityPay Point of Sale API for payment with card present devices including EMV readers and contactless POS readers.  The API is available from https://github.com/citypay/citypay-pos-api  The API makes it simple to add EMV and contactless card acceptance to iOS, Android, Tablet and desktop applicaitons using a HTTPS protocol. It segregates the complexity of payment processing from the sales environment and eliminates any necessity for the target system to handle card data. 
  *
  * OpenAPI spec version: 1.0.0
  * Contact: dev@citypay.com
